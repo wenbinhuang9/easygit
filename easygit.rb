76170594382b3572f75da931334364df2fb0a2ef
wenbin huang <ben@wenbins-MacBook-Pro.local>
2020-07-04 17:22:36 -0700

    easy git

--- easygit.rb
+++ easygit.rb
@@ -1,29 +1,65 @@
 require "open3"
 module EasyGit
 
-    # todo give full branch info
-    def self.merged_branchs
-        command = 'git branch --merged master'
-        info = command_to_a(command)
-        info
-    end
+    def self.branchs(command)
+        info = command_to_a(command).map {|e| e.strip.gsub(/^[+*]\s+/, "")}
+        info 
+    end 
+
+    def self.git_checkout_by_branch_number(number)
+        infos = local_branchs_info
+       
+        branch_name = infos[number][1]
+        puts branch_name
+
+        command_to_a('git checkout ' + branch_name)
+    end 
+
+    def self.local_branchs_info
+        command = 'git branch'
+        infos = branchs_info(branchs(command))
+
+        infos
+
+    end 
     def self.merged_branchs_info
+        command = 'git branch --merged master'
+        infos = branchs_info(branchs(command))
+
+        views infos
+        infos         
+    end 
+
+    def views(infos)
+        infos.map {|line|  puts line.join(' ')}
+    end 
+
+    def self.branchs_info(branchs)
+        branchs_info = branchs.map.with_index do |b, i|
+            arr = [i, b]
+            info = arr + branch_info(b) + ["merged"]
+
+            info 
+        end 
+        branchs_info
     end
+
+    def self.git_checkout_by_number(number)
+
+    end 
     # todo giving a branch name here 
     def self.branch_info(branch_name) 
-        command =  command = %Q(git log -n1 --date=short --format=format:"%cd %n %h %n %an %n %s" #{branch_name})
+        command = %Q(git log -n1 --date=short --format=format:"%cd %n %h %n %an %n %s" #{branch_name})
 
         info = command_to_a(command)
+
         info
-        puts info
     end
 
-    # input is  list branch detail info 
-    def self.format 
 
-    end
-
-    def self.command_to_a(command) command_output(command).split($/).map(&:strip) end 
+    def self.command_to_a(command) 
+        command_output(command).split($/).map(&:strip)
+    end 
     #Runs the passed string as a system command and returns its output.
     # If the command doesn't exit with 0 (success), then an error will be thrown, with the error
     # output as its message.
@@ -32,9 +68,4 @@ module EasyGit
 
       stdout_str
     end
-
-
 end
-
-
-
